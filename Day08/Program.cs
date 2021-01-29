9db3a7bc2b84c5fce500a6dca77e2d76c9c6efd5
Jason Robert <jason@grandcircus.co>
2021-01-28 21:40:06 -0500

    Day 8

---
+++ Day08/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Day08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // non-generic collections - Loosely Typed - Legacy
+            var arrayList = new ArrayList();
+            arrayList.Add(1234);
+            arrayList.Add("1234");
+
+            // Generic Collections - Strongly Typed - Has "Type Safety"
+            // LISTS
+            var list = new List<double>();
+            list.Add(1234);
+            // list.Add("1234"); this will give an error
+
+            var num1 = list[0]; // I know with 100% certainty this will give me an int!
+
+            // Looping through with a for loop (use .Count for lists, .Length for arrays)
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            // Looping through with a foreach loop (Preferred)
+            foreach (var num in list)
+            {
+                Console.WriteLine(num);
+            }
+
+            // DICTIONARIES
+            var dictionary = new Dictionary<double, string>();
+            dictionary.Add(1.1, "One dot one");
+            Console.WriteLine(dictionary[1.1]);
+
+            // Splitting a string
+            var sentence = "Please excuse my dear aunt Sally";
+            var words = sentence.Split();
+        }
+    }
+}
---
+++ Exercise21/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new string[1_000];
+            var index = 0;
+            do
+            {
+                Console.Write("Enter a word:");
+                result[index] = Console.ReadLine();
+                index++;
+            } while (PromptTryAgain());
+
+            PrintOutput(result);
+        }
+
+        private static void PrintOutput(string[] words)
+        {
+            foreach (var word in words)
+            {
+                if (word == null)
+                {
+                    break;
+                }
+
+                Console.Write($"{word} ");
+            }
+        }
+
+        private static bool PromptTryAgain()
+        {
+            Console.Write("Would you like to try again?");
+            var response = Console.ReadLine();
+            return response.Equals("y", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
