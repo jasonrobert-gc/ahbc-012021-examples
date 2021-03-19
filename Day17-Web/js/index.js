90ac6f7e0e9515903951b163d06e848e55eaeb52
Jason Robert <jason@grandcircus.co>
2021-03-18 21:07:23 -0400

    Some moooooar

--- Day17-Web/js/index.js
+++ Day17-Web/js/index.js
@@ -1,7 +1,37 @@
-document.querySelector("#submit").onclick = function(e) {
-    let regex = new RegExp('.*password.*');
-    let password = document.getElementById("password").value;
+document.querySelector("#password").oninput = (e) => {  //  function(e) {
+    const regex = new RegExp('.*password.*');
+    const password = document.getElementById("password").value;
     if (regex.test(password)) {
-        alert('Thats not a very strong password!');
+        document.querySelector("#passwordError").innerHTML = `"${password}" is not a very strong password!`;
+    }
+    else {
+        document.querySelector("#passwordError").innerHTML = "";
     }
 }
+
+document.querySelector("#age").oninput = (e) => {
+    const age = document.getElementById("age").value;
+    if (age < 2) {
+        document.querySelector("#ageError").innerHTML = "Age must be greater than 2";
+    }
+    else {
+        document.querySelector("#ageError").innerHTML = "";
+    }
+}
+
+let globalScopeBad = "hello world";
+
+// Self invoking functions allow you to immediately execute javascript and 
+// not put anything into global scope.
+(function() {
+    let date = new Date();
+    console.log(date);
+
+    // do {
+    //   var userInput = prompt("What is your name?");
+    //   if(userInput.length < 2){
+    //     alert('Name must contain more than 2 characters!');
+    //   }
+    // } while(userInput.length <= 2);
+    
+})();
