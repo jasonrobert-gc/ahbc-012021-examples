4fec601d5a83acf32ea99cdcf0b115c9df10a2b5
Jason Robert <jason@grandcircus.co>
2021-04-05 18:26:57 -0400

    Day 18

---
+++ Day18/Models/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Day18.Models
+{
+    public class HomeViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime RequestDate { get; set; }
+    }
+}
