5670209dd0b017dd2aaecd74f6ca433711c7c634
Jason Robert <jason@grandcircus.co>
2021-04-14 19:03:45 -0400

    c# ternary, null coalescing

--- Day20_Exercise/Controllers/HomeController.cs
+++ Day20_Exercise/Controllers/HomeController.cs
@@ -16,6 +16,19 @@ namespace Exercise.Controllers
 
         public IActionResult Index()
         {
+            //// Regular syntax
+            //var color = HttpContext.Request.Cookies["Color"];
+            //if (color == null)
+            //{
+            //    color = "#fff";
+            //}
+
+            //// ternary operator - https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/conditional-operator
+            //var color = HttpContext.Request.Cookies["Color"] == null ? "#fff" : HttpContext.Request.Cookies["Color"];
+
+            //// null coalescing - https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-coalescing-operator
+            //var color = HttpContext.Request.Cookies["Color"] ?? "#fff";
+
             var model = new IndexViewModel
             {
                 Color = HttpContext.Request.Cookies["Color"] ?? "#fff",
