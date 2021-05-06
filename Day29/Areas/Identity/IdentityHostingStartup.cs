4305a20b48d062086bc5e2461cb979353ed29a58
Jason Robert <jason@grandcircus.co>
2021-05-05 20:31:17 -0400

    Day 29

---
+++ Day29/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Day29.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Day29.Areas.Identity.IdentityHostingStartup))]
+namespace Day29.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Day29/Data/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Day29.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        public int Age { get; set; }
+
+        [PersonalData]
+        public string FirstName { get; set; }
+        
+        [PersonalData]
+        public string LastName { get; set; }
+    }
+}
